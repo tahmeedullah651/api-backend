d9942cf1a2a7bb45dcfded34751055d324eef272
Tahmeedullah <90021644+tahmeedullah651@users.noreply.github.com>
2023-08-22 11:11:26 +0500

    Update adminController.js

--- app/https/controllers/adminController/adminController.js
+++ app/https/controllers/adminController/adminController.js
@@ -12,24 +12,40 @@ function adminController() {
                 return res.status(500).json({ message: "Internal server error" });
             }
         },
-        approveService: (req, res) => {
-            ServiceModel.findOneAndUpdate({ _id: req.params.id }, { status: 'approved' }, { new: true }, (err, document) => {
-                if (err) {
-                    return res.status(500).json({ message: 'Internal server error' });
+        approveService: async (req, res) => {
+            try {
+                const doc = await ServiceModel.findOneAndUpdate({ _id: req.params.id }, { status: 'approved' }, { new: true });
+
+                if (doc) {
+                    return res.status(200).json({ message: "ok" });
                 }
-                if (!document) {
-                    return res.status(404).json({ message: 'Order not found.' });
-                }
-                return res.json({ message: 'All ok' })
-            })
+            } catch (error) {
+                console.log(error);
+                return res.status(500).json({ message: "Something went wrong" });
+
+            }
+            // ServiceModel.findOneAndUpdate({ _id: req.params.id }, { status: 'approved' }, { new: true }, (err, document) => {
+            //     if (err) {
+            //         return res.status(500).json({ message: 'Internal server error' });
+            //     }
+            //     if (!document) {
+            //         return res.status(404).json({ message: 'Order not found.' });
+            //     }
+            //     return res.json({ message: 'All ok' })
+            // })
         },
         deleteService: async (req, res) => {
-            ServiceModel.findOneAndRemove({ _id: req.params.id }, (err, document) => {
-                if (err) {
-                    return res.status(500).json({ message: 'Internal server error' });
+            try {
+                const data = await ServiceModel.findOneAndRemove({
+                    _id: req.params.id
+                }).exec();
+                if (data) {
+                    return res.status(200).json({ message: 'All ok' });
                 }
-                return res.json({ message: 'All ok' })
-            })
+            } catch (error) {
+                return res.status(500).json({ message: "Something went wrong" });
+            }
+
         }
     }
 }
